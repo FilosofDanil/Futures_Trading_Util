dbdbcb002379e4c0a51277f2ddd050d44e3a3cfd
FilosofDanil <serhio3347@gmail.com>
2023-07-10 13:53:34 +0200

    created base microservices modules

---
+++ binance_api/src/main/java/com/example/binance_api/BinanceApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.binance_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinanceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BinanceApiApplication.class, args);
+    }
+
+}
---
+++ binance_api/src/test/java/com/example/binance_api/BinanceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.binance_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinanceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
