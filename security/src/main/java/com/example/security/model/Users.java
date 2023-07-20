c9027a30765adcf0261df50294531fc80a6b1c28
FilosofDanil <serhio3347@gmail.com>
2023-07-20 14:59:56 +0200

    created User model

--- security/src/main/java/com/example/security/model/Users.java
+++ security/src/main/java/com/example/security/model/Users.java
@@ -1,4 +1,22 @@
 package com.example.security.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Users {
+    @JsonProperty("email")
+    private String email;
+    @JsonProperty("user_password")
+    private String password;
+    @JsonProperty("profile_name")
+    private String profile_name;
+    @JsonProperty("verified")
+    private Boolean verified;
+    @JsonProperty("activation_code")
+    private String activationCode;
 }
