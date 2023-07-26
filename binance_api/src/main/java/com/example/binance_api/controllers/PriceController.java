d233bc0d61c01a4a1809c0b6d16a9fa2d2e3dcc6
FilosofDanil <serhio3347@gmail.com>
2023-07-26 18:15:10 +0200

    Injected bean into controller

--- binance_api/src/main/java/com/example/binance_api/controllers/PriceController.java
+++ binance_api/src/main/java/com/example/binance_api/controllers/PriceController.java
@@ -1,6 +1,11 @@
 package com.example.binance_api.controllers;
 
+import com.example.binance_api.services.PriceService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/binance/price")
 public class PriceController {
+    private final PriceService priceService;
 
+    @GetMapping("/{marker}")
+    public ResponseEntity<String> getPrice(@PathVariable("marker") String marker) {
+        return new ResponseEntity<>(priceService.getPrice(marker), HttpStatus.OK);
+    }
 }
