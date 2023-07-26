5c8f935da0a67ebb938a4aad4f3c4780693eb632
FilosofDanil <serhio3347@gmail.com>
2023-07-26 18:18:18 +0200

    Implemented method getPrice() in a PriceComponentBean

--- binance_api/src/main/java/com/example/binance_api/components/impl/PriceComponentBean.java
+++ binance_api/src/main/java/com/example/binance_api/components/impl/PriceComponentBean.java
@@ -1,11 +1,20 @@
 package com.example.binance_api.components.impl;
 
+import com.binance.api.client.BinanceApiClientFactory;
+import com.binance.api.client.BinanceApiRestClient;
+import com.binance.api.client.domain.market.TickerStatistics;
 import com.example.binance_api.components.PriceComponent;
+
+import java.util.LinkedHashMap;
 
 public class PriceComponentBean implements PriceComponent {
     @Override
     public String getPrice(String marker) {
-
-        return null;
+        BinanceApiClientFactory factory = BinanceApiClientFactory.newInstance("API-KEY", "SECRET");
+        BinanceApiRestClient client = factory.newRestClient();
+        TickerStatistics tickerStatistics = client.get24HrPriceStatistics(marker);
+        LinkedHashMap<String, Object> map = new LinkedHashMap<>();
+        map.put("symbol", "OCEANUSDT");
+        return tickerStatistics.getLastPrice();
     }
 }
