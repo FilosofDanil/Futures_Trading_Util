02ac88c8d9f609f5800c6d659c1fe4064dea4d71
FilosofDanil <serhio3347@gmail.com>
2023-07-26 18:39:16 +0200

    PriceComponentBean now return futures price

--- binance_api/src/main/java/com/example/binance_api/components/impl/PriceComponentBean.java
+++ binance_api/src/main/java/com/example/binance_api/components/impl/PriceComponentBean.java
@@ -3,18 +3,20 @@ package com.example.binance_api.components.impl;
 import com.binance.api.client.BinanceApiClientFactory;
 import com.binance.api.client.BinanceApiRestClient;
 import com.binance.api.client.domain.market.TickerStatistics;
+import com.binance.connector.futures.client.impl.futures.Market;
 import com.example.binance_api.components.PriceComponent;
+import org.springframework.stereotype.Component;
 
 import java.util.LinkedHashMap;
 
+@Component
 public class PriceComponentBean implements PriceComponent {
     @Override
     public String getPrice(String marker) {
-        BinanceApiClientFactory factory = BinanceApiClientFactory.newInstance("API-KEY", "SECRET");
-        BinanceApiRestClient client = factory.newRestClient();
-        TickerStatistics tickerStatistics = client.get24HrPriceStatistics(marker);
-        LinkedHashMap<String, Object> map = new LinkedHashMap<>();
-        map.put("symbol", "OCEANUSDT");
-        return tickerStatistics.getLastPrice();
+        Market market = new Market("https://fapi.binance.com/fapi", "API-KEY", "SECRET", false, null) {
+        };
+        LinkedHashMap<String,Object> map= new LinkedHashMap<>();
+        map.put("symbol", marker);
+        return market.tickerSymbol(map);
     }
 }
--- binance_api/src/main/java/com/example/binance_api/services/impl/PriceServiceBean.java
+++ binance_api/src/main/java/com/example/binance_api/services/impl/PriceServiceBean.java
@@ -1,16 +1,9 @@
 package com.example.binance_api.services.impl;
 
-import com.binance.api.client.BinanceApiClientFactory;
-import com.binance.api.client.BinanceApiRestClient;
-import com.binance.api.client.domain.market.TickerStatistics;
-import com.binance.connector.futures.client.impl.UMFuturesClientImpl;
-import com.binance.connector.futures.client.impl.futures.Market;
 import com.example.binance_api.components.PriceComponent;
 import com.example.binance_api.services.PriceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.LinkedHashMap;
 
 @Service
 @RequiredArgsConstructor
