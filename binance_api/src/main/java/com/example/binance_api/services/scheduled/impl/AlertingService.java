35b2b402a34de668b065d014fe53f3ebe815791e
FilosofDanil <serhio3347@gmail.com>
2023-07-30 13:25:30 +0200

    implemented basically scheduled task logic for an AlertingService

--- binance_api/src/main/java/com/example/binance_api/services/scheduled/impl/AlertingService.java
+++ binance_api/src/main/java/com/example/binance_api/services/scheduled/impl/AlertingService.java
@@ -1,18 +1,37 @@
 package com.example.binance_api.services.scheduled.impl;
 
-import com.example.binance_api.client.DataClient;
+import com.example.binance_api.components.PriceComponent;
+import com.example.binance_api.models.Alerts;
+import com.example.binance_api.models.Users;
+import com.example.binance_api.services.AlertService;
+import com.example.binance_api.services.impl.UserServiceBean;
 import com.example.binance_api.services.scheduled.IScheduled;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Objects;
+
 @Service
 @RequiredArgsConstructor
 public class AlertingService implements IScheduled {
-    private final DataClient client;
+    private final UserServiceBean userService;
+    private final AlertService alertService;
+    private final PriceComponent priceComponent;
 
 
     @Override
     public void scheduledTask() {
-
+        List<Users> users = userService.getAll();
+        users.forEach(user -> {
+            List<Alerts> alerts = alertService.getAll(user);
+            alerts.forEach(alert -> {
+                Double currentPrice = 0.0;
+                if (Objects.equals(currentPrice, alert.getPrice())) {
+                    alert.setCrossed(true);
+                    alertService.update(user, alert, alert.getId());
+                }
+            });
+        });
     }
 }
